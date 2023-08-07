90463b3a846bdcb8de9b272ae4fd786acc336509
shixiansi <1765629830@qq.com>
2023-08-07 23:31:54 +0800

    水一个没用的戳一戳功能

--- apps/chuo.js
+++ apps/chuo.js
@@ -1,4 +1,5 @@
 import Base from '../model/base/Base.js'
+import lodash from 'lodash'
 export default class chuo extends Base {
     constructor() {
         super({
@@ -15,6 +16,17 @@ export default class chuo extends Base {
     }
 
     async poke(e) {
-
+        if (!this.Cfg.isOpen) return false
+        let groupNumber = this.e.group_id;
+        /** 判断戳的是不是自己 */
+        if (e.target_id == e.self_id) {
+            let imgData = this.Data.getDataJson(`groupface/${groupNumber}-face`) || [];
+            if (imgData.length == 0) {
+                return this.reply("还没有在该群学习过表情包");
+            } else {
+                let imageUrl = imgData[lodash.random(0, imgData.length - 1)]
+                this.e.reply(imageUrl.content);
+            }
+        }
     }
 }
