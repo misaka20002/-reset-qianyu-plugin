e1dedb26a85376fde4e861a0b7ec70a968dc7e53
shixiansi <1765629830@qq.com>
2023-09-03 00:40:21 +0800

    修复快手解析

--- model/kuaishou.js
+++ model/kuaishou.js
@@ -14,14 +14,16 @@ export default class kuaishou extends Base {
     }
 
     async getKuaishouVideo(url) {
-        console.log(this.ck);
         let headers = {
             referer: "www.kuaishou.com",
             cookie: this.ck || 'did=web_aebc21c90cd01acbe31d9179c9b78952; didv=1692807076659; kpf=PC_WEB; kpn=KUAISHOU_VISION; clientid=3',
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.62"
         }
-        let res = await new this.networks({ url: 'https://www.kuaishou.com/f/X-nfcdq3Mku41gm' }).getfetch()
-        url = res.url
+        if (url.includes('v.kuaishou.com')) {
+            let res = await new this.networks({ url: url, headers: headers }).getfetch()
+            url = res.url
+        }
+
         let data = await new this.networks({
             url: url,
             headers: headers, type: "text"
