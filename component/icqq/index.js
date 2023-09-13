8706d813101da5d774ffc29fd941b08a3ee6c634
shixiansi <1765629830@qq.com>
2023-09-13 16:32:48 +0800

    修复了插件的一些兼容性问题

--- component/icqq/index.js
+++ component/icqq/index.js
@@ -1,3 +1,9 @@
-import { segment } from "icqq"
+let icqq;
+try {
+    icqq = await import("icqq");
+} catch (err) {
+    icqq = await import("oicq");
+}
+let segment = icqq.segment;
 export { segment }
 
