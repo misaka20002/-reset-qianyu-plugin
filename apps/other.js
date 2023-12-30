039a590bfe658cd894632efd0d0581271d6ca98b
misaka20002 <misakamisaka20002@gmail.com>
2023-12-30 19:57:20 +0800

    禁用#撤回

--- apps/other.js
+++ apps/other.js
@@ -11,11 +11,11 @@ export default class other extends Base {
                     fnc: 'jxtu',
                     log: false
                 },
-                {
-                    reg: '^#撤回',
-                    fnc: 'che',
-                    permission: 'master'
-                },
+                // {
+                //     reg: '^#撤回',
+                //     fnc: 'che',
+                //     permission: 'master'
+                // },
                 // {
                 //     reg: '^#取直链',
                 //     fnc: 'zhilian'
@@ -36,18 +36,18 @@ export default class other extends Base {
         }        
     }
 
-    async che(e) {
-        if (!e.source) {
-            return e.reply("不存在消息源！")
-        }
-        if (e.source.user_id == e.self_id) {
-            let msgid = (await e.group.getChatHistory(e.source.seq, 1))[0].message_id
-            let res = await e.group.recallMsg(msgid)
-            if (!res) {
-                this.reply("伦家不是管理员，不能撤回超过2分钟的消息呢~")
-            }
-        }
-    }
+    // async che(e) {
+    //     if (!e.source) {
+    //         return e.reply("不存在消息源！")
+    //     }
+    //     if (e.source.user_id == e.self_id) {
+    //         let msgid = (await e.group.getChatHistory(e.source.seq, 1))[0].message_id
+    //         let res = await e.group.recallMsg(msgid)
+    //         if (!res) {
+    //             this.reply("伦家不是管理员，不能撤回超过2分钟的消息呢~")
+    //         }
+    //     }
+    // }
 
     // async zhilian(e) {
     //     if (!e.source) {
